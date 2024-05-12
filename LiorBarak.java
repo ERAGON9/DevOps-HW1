a3a3380993d8a1dd1b22c04cab4e7dbcce5972c4
ERAGON9 <liorbarak99@gmail.com>
2024-05-12 14:28:55 +0300

    Update the file LiorBarak.java

--- LiorBarak.java
+++ LiorBarak.java
@@ -1,7 +1,7 @@
 public class LiorBarak{
     public static void main(String[] args){
         for(int i=1;i<=10;i++){
-            System.out.println("Hello Lior Barak " + i);
+            System.out.println("Diffrent print: Hello again Lior Barak " + i);
         }
     }
 }
